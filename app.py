35cfe57a55a5d451e505eb6343ba67815eadeb5b
Mark Hawkins <mark.hawkins@gmail.com>
2024-09-17 05:58:11 +0100

    Files for section 1

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/your-url", methods=["GET", "POST"])
+def yoururl():
+    if request.method == "POST":
+        return render_template("your_url.html", code=request.form["code"])
+    return redirect(url_for("home"))
