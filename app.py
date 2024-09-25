434c33a3d1b8d6f47e8eeb8becab9e207ffcefcb
Mark Hawkins <mark.hawkins@gmail.com>
2024-09-25 06:15:01 +0000

    Updated app.py as per Chapter 2 Section 4

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import os.path
+
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
@@ -11,5 +14,17 @@ def home():
 @app.route("/your-url", methods=["GET", "POST"])
 def yoururl():
     if request.method == "POST":
+        urls = {}
+
+        if os.path.exists("urls.json"):
+            with open("urls.json") as url_file:
+                urls = json.load(url_file)
+
+        if request.form["code"] in urls.keys():
+            return redirect(url_for("home"))
+
+        urls[request.form["code"]] = {"url": request.form["url"]}
+        with open("urls.json", "w") as url_file:
+            json.dump(urls, url_file)
         return render_template("your_url.html", code=request.form["code"])
     return redirect(url_for("home"))
